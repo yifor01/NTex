75cfd8928c53476b04c93880ec02ea422aa5795f
yifor01 <howard99664411@gmail.com>
2020-02-22 13:10:08 +0800

    每日更新範本

--- NTex/sample.py
+++ NTex/sample.py
@@ -1,12 +1,19 @@
+import os
+import pandas as pd
+pd.options.display.max_columns = 10
+os.chdir(r'D:\jobs\exchange_rate\NTex\NTex')
 from ExchangeRate import NTex
 
-NTex().currencies()
+QQ = NTex()._currencies()
 NTex().now_all()
 
+for cur in NTex()._currencies().keys():
+    try:
+        CUR = NTex(cur)
+        CUR.plot(2020)
+    except:
+        print('error:',cur)
 
-USD = NTex(currency='USD')
-USD.now()
-USD.news()
-USD.history()
-USD.plot(2010)
-USD._year(2020)
+QQ = NTex()
+QQ.gold()
+QQ.gold_now()
